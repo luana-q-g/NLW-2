8352048d9f9a4538c0d56b9a6e1f9084173f1c2f
luana-q-g <luana.garcia@catijr.com.br>
2020-08-08 22:14:11 -0300

    Server part of NLW

---
+++ server/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+import routes from './routes';
+import cors from 'cors';
+
+const app = express();
+
+app.use(cors());
+//para o express interpretar json
+app.use(express.json());
+app.use(routes);
+
+//localhost:3333
+app.listen(3333);
